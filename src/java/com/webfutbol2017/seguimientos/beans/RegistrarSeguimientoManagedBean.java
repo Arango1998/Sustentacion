4e5e1256c14fb62705278baab29449d53d6ce139
camila <camila@sebastian>
2017-03-28 16:17:01 -0500

    William

---
+++ src/java/com/webfutbol2017/seguimientos/beans/RegistrarSeguimientoManagedBean.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.webfutbol2017.seguimientos.beans;
+
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+
+/**
+ *
+ * @author camila
+ */
+@Named(value = "registrarSeguimientoManagedBean")
+@RequestScoped
+public class RegistrarSeguimientoManagedBean {
+
+    /**
+     * Creates a new instance of RegistrarSeguimientoManagedBean
+     */
+    public RegistrarSeguimientoManagedBean() {
+    }
+    
+}
